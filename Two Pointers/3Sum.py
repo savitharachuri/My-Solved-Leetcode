e89b67ee263e5159affb4d9055dcd854ab2cdf34
Savitha Rachuri <savitharachuri223@gmail.com>
2025-01-17 18:32:35 -0500

    Update 3Sum.py

--- Two Pointers/3Sum.py
+++ Two Pointers/3Sum.py
@@ -1,1 +1,19 @@
+# 15. 3Sum 
+# https://leetcode.com/problems/3sum/description/
 
+def three_sum(nums, target):
+  nums.sort()
+  for i in range(0, len(nums)-2):
+    low = i + 1
+    right = len(nums) - 1
+    while low < right:
+      triplet = nums[i] + nums[low] + nums[right]
+      if triplet == target:
+        return True
+      elif triplet < target:
+        low += 1
+      else:
+        right -= 1
+  return False
+      
+    
