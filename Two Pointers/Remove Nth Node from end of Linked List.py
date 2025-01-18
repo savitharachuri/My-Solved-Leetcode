b0e7b798556dc8a42627119fc6f082271edacec8
Savitha Rachuri <savitharachuri223@gmail.com>
2025-01-17 19:39:48 -0500

    Update Remove Nth Node from end of Linked List.py

--- Two Pointers/Remove Nth Node from end of Linked List.py
+++ Two Pointers/Remove Nth Node from end of Linked List.py
@@ -1,1 +1,20 @@
+def remove_node_from_end(head, n):
+  right = head
+  left = head
 
+  for i in range(n):
+    right = right.next
+
+  if not right: # it means if right == None
+    return head.next 
+
+  while right.next != None:
+    right = right.next
+    left = left.next
+
+  left.next = left.next.next
+
+  return head
+  
+
+  
